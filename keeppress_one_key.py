b6f7dd9e659a3d1bac5ba092758c344e0b49b102
李奕霖 <lyl@yunshan.cn>
2022-08-02 17:16:12 +0800

    all test 0802

--- keeppress_one_key.py
+++ keeppress_one_key.py
@@ -9,9 +9,10 @@ while True:
         s = subprocess.Popen("adb shell input tap {} {}".format(x,y))
         time.sleep(0.3)
         s = subprocess.Popen("adb shell input tap {} {}".format(1074,419))
-        time.sleep(0.3)
-
+        time.sleep(0.2)
         s = subprocess.Popen("adb shell input tap {} {}".format(986,504))
+        time.sleep(0.2)
+        s = subprocess.Popen("adb shell input tap {} {}".format(933,370))
         time.sleep(0.3)
 
         tt = time.time()
--- keytest.py
+++ keytest.py
@@ -11,34 +11,90 @@ def check_key_code(code):
     """
     attack-j
     """
-    if code.name == 'j' and code.event_type == 'down':
-        x,y = 1117,554
-        t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
-    """
-    1/2/3/4 技能
-    """
-    if code.name == '1' and code.event_type == 'down':
-        x,y = 1117,554
-        t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
-    if code.name == '2' and code.event_type == 'down':
-        x,y = 1117,554
-        t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
-    if code.name == '3' and code.event_type == 'down':
-        x,y = 1117,554
-        t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
-    if code.name == '4' and code.event_type == 'down':
-        x,y = 1117,554
-        t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
-
-
-
-
     print(code.name, code.event_type)
 
+    # if code.name == 'j' and code.event_type == 'down':
+    #     x,y = 1117,554
+    #     t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
+    # """
+    # 1/2/3/4 技能
+    # """
+    # if code.name == '1' and code.event_type == 'down':
+    #     x,y = 1117,554
+    #     t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
+    # if code.name == '2' and code.event_type == 'down':
+    #     x,y = 1117,554
+    #     t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
+    # if code.name == '3' and code.event_type == 'down':
+    #     x,y = 1117,554
+    #     t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
+    # if code.name == '4' and code.event_type == 'down':
+    #     x,y = 1117,554
+    #     t = subprocess.Popen("adb shell input tap {} {}".format(x,y))
+t_fps = 1/30
+t = time.time()
+class Key_Manager():
+    def __init__(self):
+        self.key_pool = set()
+    def key_callback(self,code):
+        if time.time()-t<t_fps:
+            return
+
+        if code.event_type == 'down':
+            self.key_pool.add(code.name)
+        if code.event_type == "up":
+            if self.key_pool.__contains__(code.name):
+                self.key_pool.remove(code.name)
+
+        if 'a' in self.key_pool and 's' in self.key_pool:
+            print('as')
+        if 'a' in self.key_pool and 'w' in self.key_pool:
+            print('aw')
+        if 'w' in self.key_pool and 's' in self.key_pool:
+            print('ws')
+        if 'w' in self.key_pool and 'd' in self.key_pool:
+            print('wd')
+        if 'q' in self.key_pool:
+            print('a')
+        if 'q' in self.key_pool:
+            print('s')
+        if 'q' in self.key_pool:
+            print('d')
+        if 'q' in self.key_pool:
+            print('w')
+        if 'q' in self.key_pool:
+            print('q')
+        if 'f' in self.key_pool:
+            print('f')
+        if 'j' in self.key_pool:
+            print('j')
+        if '1' in self.key_pool:
+            print('1')
+        if '2' in self.key_pool:
+            print('2')
+        if '3' in self.key_pool:
+            print('3')
+        if '4' in self.key_pool:
+            print('4')
+
+        if 'esc' in self.key_pool:
+            print('esc')
+
+    def tap_func(self,x,y):
+        s = subprocess.Popen("adb shell input tap {} {}".format(x, y))
 
 
+
+
+class A():
+    def __init__(self):
+        self.Global_p1= 0
+    def keytest(self):
+        self.Global_p1+=1
 
 
 if __name__ == '__main__':
-    keyboard.hook(lambda e: check_key_code(e))
+    km = Key_Manager()
+    keyboard.hook(lambda e: km.key_callback(e))
     keyboard.wait('Ctrl')
+
